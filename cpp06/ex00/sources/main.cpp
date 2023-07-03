4d8450a4a1c8465f6cd03c1dab8127d7d657a9c7
Humberto Arndt <humberto.arndt@gmail.com>
2023-07-03 10:42:49 -0300

    created ex00 folder

--- cpp06/ex00/sources/main.cpp
+++ cpp06/ex00/sources/main.cpp
@@ -6,11 +6,22 @@
 /*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 16:30:23 by harndt            #+#    #+#             */
-/*   Updated: 2023/06/29 18:45:32 by harndt           ###   ########.fr       */
+/*   Updated: 2023/06/30 11:47:15 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+
+// TODOS
+// [] Create static class ScalarConverter andh its Orthodox Canonical Form Methods
+// [] Crete Makefile
+// [] Understand the different casts in CPP
+// [] Casting in C++
+// https://www.youtube.com/watch?v=pWZS1MtxI-A&pp=ygUIY3BwIGNhc3Q%3D
+// [] Dynamic Cast in C++
+// https://www.youtube.com/watch?v=CiHfz6pTolQ&pp=ygUIY3BwIGNhc3Q%3D
+// [] Implicit Conversion in C++
+// https://www.youtube.com/watch?v=Rr1NX1lH3oE&pp=ygUIY3BwIGNhc3Q%3D
 
 int	main(void)
 {
