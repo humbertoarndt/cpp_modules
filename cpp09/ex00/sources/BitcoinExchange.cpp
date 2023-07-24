4014b5a1733514fc316ee0f2c1287711453d5995
Humberto Arndt <humberto.arndt@gmail.com>
2023-07-24 13:49:55 -0300

    update: created checkDate() to validate dates in YYYY-MM-DD format

--- cpp09/ex00/sources/BitcoinExchange.cpp
+++ cpp09/ex00/sources/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 13:06:39 by harndt            #+#    #+#             */
-/*   Updated: 2023/07/24 13:28:58 by harndt           ###   ########.fr       */
+/*   Updated: 2023/07/24 13:48:41 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,29 @@ BitcoinExchange &BitcoinExchange::operator = (BitcoinExchange const &rhs)
 // =============================================================================
 
 /**
+ * @brief Checks if a date is in the YYYY-MM-DD format.
+ * 
+ * @param date The date to check.
+ * @return true When date is in the YYYY-MM-DD format.
+ * @return false When date isn't in the YYYY-MM-DD format.
+ */
+static bool	checkDate(std::string date)
+{
+	if (date.size() != 10)
+		return (false);
+	if (date[5] != '0' && date[5] != 1) // Month check
+		return (false);
+	if (date[8] > '3' || date[8] < '0') // Day check
+		return (false);
+	for (size_t i = 0; i < date.size(); i++)
+	{
+		if(!std::isdigit(date[i]) && date[i] != '-')
+			return (false);
+	}
+	return (true);
+}
+
+/**
  * @brief Reads and stores the values from 'data.csv' file in 
  * 'BitcoinExchange::_map'
  * 
@@ -97,6 +120,8 @@ void	BitcoinExchange::readData(void)
 
 		if (!std::getline(iss, date, ',') || !std::getline(iss, valueStr))
 			continue ;
+		if (!checkDate(date))
+			return ;
 		double	value = atof(valueStr.c_str());
 		BitcoinExchange::_map.insert(std::pair <std::string, double> (date, value));
 	}
